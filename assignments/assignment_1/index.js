4b989548620ccaf7001f5ea1a77887f6cb66fccc
HrushikeshPune <hrushikeshpune1997@gmail.com>
2022-02-20 11:33:10 +0530

    Assignment-1

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,18 @@
 function getNameFromCommandLine() {
-    // Write you code here, name should be taken as args in process.argv
+    var name =process.argv[process.argv.length-1];
+    return name
 }
-
 function getNameFromEnv() {
-    // Write your code here
+    return process.env.name
+
 }
 
 function getNameFromReadLine() {
-    // Write your code here
+    const readline = require('readline');
+    readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
 }
 
 module.exports = {
