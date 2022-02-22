0eab8016321038182ac510ffa4d605be6b360aaf
HrushikeshPune <hrushikeshpune1997@gmail.com>
2022-02-22 17:04:40 +0530

    Assignment-2

--- assignments/assignment_2/index.js
+++ assignments/assignment_2/index.js
@@ -1,13 +1,13 @@
-const http =require("http");
-const fs =require("fs")
-fs.writeFile("index.html","<h1>Hello world <h1>",err =>{
+const http = require("http");
+const fs = require("fs")
+fs.writeFile("index.html", "<h1>Hello world <h1>", err => {
     console.log(err)
 })
-const server =http.createServer((req,res) =>{
-    fs.readFile("index.html",{encoding:"utf-8"},(err,data)=>{
+const server = http.createServer((req, res) => {
+    fs.readFile("index.html", { encoding: "utf-8" }, (err, data) => {
         res.end(data);
     })
 })
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log("server is responding")
 })
